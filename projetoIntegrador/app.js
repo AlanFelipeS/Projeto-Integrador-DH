92d49d27bbbc13a493b01b2c1c7ecbd95c92e672
Your Name <souzafalan@gmail.com>
2022-03-01 11:19:36 -0300

    roteamento

--- projetoIntegrador/app.js
+++ projetoIntegrador/app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var areaAlunoRouter = require('./routes/areaAluno');
+var loginRouter = require('./routes/login');
+var cadastroAlunoRouter = require('./routes/cadastroAluno');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/areaAluno', areaAlunoRouter);
+app.use('/login', loginRouter);
+app.use('/cadastroAluno', cadastroAlunoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
