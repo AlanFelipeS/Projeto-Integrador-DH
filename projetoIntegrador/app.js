db9971dac061c53551e92148bd8735e8063dba82
“AssFerj” <assisjuniorcam@gmail.com>
2022-03-06 21:46:00 -0300

    criado pasta src e adicionado pastas, auterado app

--- projetoIntegrador/app.js
+++ projetoIntegrador/app.js
@@ -4,14 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var areaAlunoRouter = require('./routes/areaAluno');
+var indexRouter = require('./src/routes/index');
+var usersRouter = require('./src/routes/users');
+var areaAlunoRouter = require('./src/routes/areaAluno');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
