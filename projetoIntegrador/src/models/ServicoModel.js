11d7c3fe140ca60cf1a5e1d4b31a4b3071e9961d
Your Name <souzafalan@gmail.com>
2022-03-07 20:46:39 -0300

    fix branch

--- projetoIntegrador/src/models/ServicoModel.js
+++ projetoIntegrador/src/models/ServicoModel.js
@@ -7,39 +7,40 @@ const fs = require('fs');
 module.exports = {
     servicos: [
     { 
-        id1:
+        id1:  1,
         nome: 'Consultoria Fitness',
         valor: 'R$ 150,00',
         descricao: "Consultoria personalizada de treinos"
     },
     {
-            id2:
+            id2: 2,
             nome: 'Treino com personal',
             valor: 'R$ 180,00',
             descricao: "Treino com personal individual"
     },
     {
-            id3:
-            nome: 'PLano alimentar',
+            id3: 3,
+            nome: 'Plano alimentar',
             valor: 'R$ 550,00',
             descricao: "Pacote de treino 5x na semana com adicional de plano alimentar exclusivo, feito pelo nosso nutricionista"
-    },
-],
-}
+    }
+]
+};
 
 index () {
     return this.servicos
-},
+}
 
 criar ( {nome, valor, descricao}, File){
+
     if (!nome || !valor || !descricao) return
     const filePatch = File ? path.join('/images/uploads', File.filename) : ''
     this.servicos.push({ id: uuid(), nome, valor, descricao, image: filePatch});
-},
+}
 
 buscar (id) {
     return this.servicos.find(servico => servico.id == id);
-},
+};
 
 atualizar (id, {nome, valor, descricao }) {
     if (!id) return
@@ -49,7 +50,7 @@ atualizar (id, {nome, valor, descricao }) {
     servico.nome = nome;
     servico.valor = valor;
     servico.descricao = descricao;
-},
+}
 
 deletar (id) {
     if (!id) return
@@ -60,7 +61,7 @@ deletar (id) {
 
     const index = this.servicos.findIndex(servico => servico.id == id);
 }
-}
+
 
 
 
