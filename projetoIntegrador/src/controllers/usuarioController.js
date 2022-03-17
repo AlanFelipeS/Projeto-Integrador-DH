92901ee4a9cd01181925cf7c9f795ac58138d8b8
Your Name <souzafalan@gmail.com>
2022-03-16 21:54:29 -0300

    rota de usuario

---
+++ projetoIntegrador/src/controllers/usuarioController.js
@@ -0,0 +1,8 @@
+// const UsuarioModel = require('../models/MohaveModel');
+
+const UsuarioController = {
+    index: (req, res) => {
+        res.render('cadastroUsuario');
+    }
+}
+module.exports = UsuarioController;
