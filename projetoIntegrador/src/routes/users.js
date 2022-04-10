c6ed99dad5e5e8107356acf4901beeaf0fe6291a
Luana Morais <93208310+Luanapsmorais@users.noreply.github.com>
2022-04-10 17:19:54 -0300

    adding sessions and cookies pt 1

--- projetoIntegrador/src/routes/users.js
+++ projetoIntegrador/src/routes/users.js
@@ -6,6 +6,8 @@ const UsuarioController = require('../controllers/usuarioController');
 
 router.get('/cadastro', UsuarioController.form);
 
+//router.post('/cadastro', UsuarioController.criar);
+
 module.exports = router;
 
 
